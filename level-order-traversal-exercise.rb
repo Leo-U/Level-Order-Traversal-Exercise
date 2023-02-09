b44a90969ee1e7079241b2b4663e802121933fc8
Joel Howard <joelhoward2@gmail.com>
2023-02-09 15:59:59 -0500

    fix indenting

--- level-order-traversal-exercise.rb
+++ level-order-traversal-exercise.rb
@@ -6,44 +6,47 @@ class Node
   @left = left
   @right = right
   end
-  end
+
+end
   
-  def level_order(root)
+def level_order(root)
   return if root.nil?
   queue = []
   queue.push(root)
   while !queue.empty?
-  current = queue.shift
-  print "#{current.data} "
-  queue.push(current.left) unless current.left.nil?
-  queue.push(current.right) unless current.right.nil?
+    current = queue.shift
+    print "#{current.data} "
+    queue.push(current.left) unless current.left.nil?
+    queue.push(current.right) unless current.right.nil?
   end
-  end
-  
-  def insert(root, data)
+end
+
+def insert(root, data)
   if root.nil?
-  root = Node.new(data)
+    root = Node.new(data)
   elsif data <= root.data
-  root.left = insert(root.left, data)
+    root.left = insert(root.left, data)
   else
-  root.right = insert(root.right, data)
+    root.right = insert(root.right, data)
   end
   root
-  end
-  
-  # Creating an example tree
-  # M
-  # / \
-  # B Q
-  # / \ \
-  # A C Z
-  root = nil
-  root = insert(root, 'M')
-  root = insert(root, 'B')
-  root = insert(root, 'Q')
-  root = insert(root, 'Z')
-  root = insert(root, 'A')
-  root = insert(root, 'C')
-  
-  # Print nodes in level order
-  level_order(root)
+end
+
+# Creating an example tree
+# Mreturn if root.nil?
+queue = []
+queue.push(root)
+# / \
+# B Q
+# / \ \
+# A C Z
+root = nil
+root = insert(root, 'M')
+root = insert(root, 'B')
+root = insert(root, 'Q')
+root = insert(root, 'Z')
+root = insert(root, 'A')
+root = insert(root, 'C')
+
+# Print nodes in level order
+level_order(root)
